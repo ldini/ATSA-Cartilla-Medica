dde14618967d3863b6737a3d8fa475e0714dd9f9
Leandro <leandro.david.dini@gmail.com>
2023-10-05 12:32:55 -0300

    filtros modificados

--- src/Components/CardsConteiner/CarsConteiner.jsx
+++ src/Components/CardsConteiner/CarsConteiner.jsx
@@ -3,146 +3,181 @@ import Card from '../Card/Card';
 import './CarsContainer.css';
 
 const CarsContainer = () => {
-  const [filterType, setFilterType] = useState('');
-  const [filterValue, setFilterValue] = useState('');
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [showGuardia, setShowGuardia] = useState(false);
+
+  // Estados para los filtros seleccionados
+  const [data,setData] = useState([])
+  const [selectedZone, setSelectedZone] = useState('');
+  const [selectedInstitution, setSelectedInstitution] = useState('');
+  const [selectedProvider, setSelectedProvider] = useState('');
+  const [selectedSpeciality, setSelectedSpeciality] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/prestador/detail');
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+
+    }
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const filteredData = data.filter((medico) => {
-      const lowerCaseFilterValue = filterValue.toLowerCase();
-      if (filterType === 'city') {
-        return medico.institucion_zona.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'specialty') {
-        return medico.especialidad_nombre.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'institution') {
-        return medico.institucion_nombre.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'provider') {
-        return (medico.prestador_apellido + ' ' + medico.prestador_nombre).toLowerCase().includes(lowerCaseFilterValue);
-      }
-      return false;
-    });
+  // Obtener las opciones únicas de zona, institución y prestador
+  const zones = [...new Set(data.map((item) => item.institucion_zona))];
+  const [availableInstitutionsByZone, setAvailableInstitutionsByZone] = useState([]);
+  const [availableProvidersByZone, setAvailableProvidersByZone] = useState([]);
+  const [availableSpecialityByZone, SetAvailableSpecialityByZone] = useState([]);
 
-    setFilteredData(filteredData);
-  }, [filterValue, filterType, data]);
+  // Función para aplicar los filtros
+  const applyFilters = () => {
+    let filtered = data;
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('http://107.180.104.190:3000/prestador/detail');
-      // const response = await fetch('http://192.168.0.8:3002/prestador/detail');
-      const jsonData = await response.json();
-      console.log(jsonData);
-      setData(jsonData);
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setIsLoading(false);
+    if (selectedZone) {
+      if(selectedZone !== 'all')
+        filtered = filtered.filter((item) => item.institucion_zona === selectedZone);   
+      const institutionsInZone = [...new Set(filtered.map((item) => item.institucion_nombre))];
+      setAvailableInstitutionsByZone(institutionsInZone);
     }
-  };
+    
 
-  const handleFilterTypeChange = (event) => {
-    const newFilterType = event.target.value;
-    setFilterType(newFilterType);
-    setFilterValue('');
-    setShowGuardia(false); 
-    setFilteredData([])
-  };
-
-  const handleFilterValueChange = (event) => {
-    setFilterValue(event.target.value);
-  };
-
-
-  const getFilterOptions = () => {
-    if (filterType === 'city') {
-      return [...new Set(data.map((medico) => medico.institucion_zona))];
-    } else if (filterType === 'specialty') {
-      return [...new Set(data.map((medico) => medico.especialidad_nombre))];
-    } else if (filterType === 'institution') {
-      return [...new Set(data.map((medico) => medico.institucion_nombre))];
-    } else if (filterType === 'provider') {
-      return [...new Set(data.map((medico) => medico.prestador_apellido + ' ' + medico.prestador_nombre))];
+    if (selectedInstitution) {
+      if(selectedInstitution !== 'all')
+        filtered = filtered.filter((item) => item.institucion_nombre === selectedInstitution);
+      const specialityInZone = [...new Set(filtered.map((item) => item.especialidad_nombre))];
+      SetAvailableSpecialityByZone(specialityInZone);
     }
-    return [];
-  };
 
-  const getInstitutionsForProvider = (providerName) => {
-    return data
-      .filter((medico) => (medico.prestador_apellido + ' ' + medico.prestador_nombre) === providerName)
-      .map((medico) => medico.institucion_nombre);
+    if (selectedSpeciality) {
+      if(selectedSpeciality !== 'all')
+        filtered = filtered.filter((item) => item.especialidad_nombre === selectedSpeciality);
+      const providersInZone = [...new Set(filtered.map((item) => item.prestador_apellido + ' ' + item.prestador_nombre ))];
+      setAvailableProvidersByZone(providersInZone);
+    }
+
+    if (selectedProvider) {
+      filtered = filtered.filter((item) => item.prestador_apellido + ' ' + item.prestador_nombre === selectedProvider);
+    }
+
+    setFilteredData(filtered);
   };
 
   useEffect(() => {
-    const updatedFilteredData = data.filter((medico) => {
-      const lowerCaseFilterValue = filterValue.toLowerCase();
-      if (filterType === 'city') {
-        return medico.institucion_zona.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'specialty') {
-        return medico.especialidad_nombre.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'institution') {
-        return medico.institucion_nombre.toLowerCase().includes(lowerCaseFilterValue);
-      } else if (filterType === 'provider') {
-        return (medico.prestador_apellido + ' ' + medico.prestador_nombre).toLowerCase().includes(lowerCaseFilterValue);
-      }
-      return false;
-    });
-    setFilteredData([]);
-    setFilteredData(updatedFilteredData); // Sobrescribir con los datos filtrados actuales
-  }, [filterValue, filterType, data]);
+    applyFilters();
+  }, [selectedZone, selectedInstitution, selectedProvider,selectedSpeciality]);
 
-  const shouldShowCards = filterValue !== '';
 
   return (
     <div>
-  <h2>Cartilla Médica</h2>
-  {isLoading ? (
-    <div className="loading-spinner">Cargando...</div>
-  ) : (
-    <div className="filter-container select-container">
-      <div className="filter">
-          <label htmlFor="filterType" className='select-label'></label>
-          <select id="filterType" className='select' value={filterType} onChange={handleFilterTypeChange}>
-            <option selected value="">--- Seleccionar Filtro ---</option>
-            <option value="specialty">Especialidad</option>
-            <option value="city">Zona</option>
-            <option value="institution">Centro de Salud</option>
-            <option value="provider">Especialista</option>
+      <h2>Cartilla Médica</h2>
+      <div className="filter-container select-container">
+        <div className="filter">
+          <label htmlFor="filterZone" className="select-label">
+            Zona:
+          </label>
+          <select
+            id="filterZone"
+            className="select"
+            value={selectedZone}
+            onChange={(e) => {
+              setSelectedZone(e.target.value);
+              setSelectedInstitution('');
+              setSelectedProvider(''); 
+              setSelectedSpeciality('');
+            }}
+            
+          >
+            <option value="">--- Seleccionar Zona ---</option>
+            <option value="all">Todas las zonas</option>
+            {zones.map((zone) => (
+              <option key={zone} value={zone}>
+                {zone}
+              </option>
+            ))}
           </select>
 
-        <label htmlFor="filterType" className='select-label'></label>
-        <select
-          className='select'
-          value={filterValue}
-          onChange={handleFilterValueChange}
-          disabled={!filterType}
-        >
-          <option value="">Todas las {filterType === 'specialty' ? 'especialidades' : filterType === 'city' ? 'ciudades' : filterType === 'institution' ? 'instituciones' : 'prestadores'}</option>
-          {getFilterOptions().map((option) => (
-            <option key={option} value={option}>{option}</option>
-          ))}
-        </select>
+          <label htmlFor="filterInstitution" className="select-label">
+            Centro:
+          </label>
+          <select
+            id="filterInstitution"
+            className="select"
+            value={selectedInstitution}
+            onChange={(e) => setSelectedInstitution(e.target.value)}
+          >
+            <option value="">--- Seleccione un Centro ---</option>
+            <option value="all"> Todos los Centros </option>
+            {selectedZone ? (
+              availableInstitutionsByZone.map((institution) => (
+                <option key={institution} value={institution}>
+                  {institution}
+                </option>
+              ))
+            ) : (
+              <option value="" disabled>
+                Selecciona una Zona primero
+              </option>
+            )}
+          </select>
+
+          <label htmlFor="filterSpeciality" className="select-label">
+            Especialidad:
+          </label>
+          <select
+            id="filterSpeciality"
+            className="select"
+            value={selectedSpeciality}
+            onChange={(e) => setSelectedSpeciality(e.target.value)}
+          >
+            <option value="">--- Seleccione Especialidad ---</option>
+            <option value="all">Todos las Especialidades</option>
+            {selectedInstitution ? (
+              availableSpecialityByZone.map((speciality) => (
+                <option key={speciality} value={speciality}>
+                  {speciality}
+                </option>
+              ))
+            ) : (
+              <option value="" disabled>
+                Selecciona un Centro Primero
+              </option>
+            )}
+          </select>
+
+          <label htmlFor="filterProvider" className="select-label">     
+            Especialista:
+          </label>
+          <select
+            id="filterProvider"
+            className="select"
+            value={selectedProvider}
+            onChange={(e) => setSelectedProvider(e.target.value)}
+          >
+            <option value="">Todos los Especialistas</option>
+            {selectedInstitution ? (
+              availableProvidersByZone.map((provider) => (
+                <option key={provider} value={provider}>
+                  {provider}
+                </option>
+              ))
+            ) : (
+              <option value="" disabled>
+                Selecciona un Centro Primero
+              </option>
+            )}
+          </select>
+
+        </div>
       </div>
-      <div className="checkGuardia">
-      <input
-            type="checkbox"
-            id="showGuardia"
-            checked={showGuardia}
-            onChange={() => setShowGuardia(!showGuardia)}
-          />
-          <label htmlFor="showGuardia">Guardia</label>
-      </div>
-    </div>
-  )}
-  {shouldShowCards && (
-    <div className="card-container">
+
+      <div className="card-container">
       {filteredData.map((medico, index) => (
-        (showGuardia || medico.prestador_nombre !== 'guardia') && (
+         (
           <Card
             key={index}
             nombre={medico.prestador_apellido}
@@ -152,15 +187,13 @@ const CarsContainer = () => {
             telefono_numero={medico.telefonos}
             institucion_direccion={medico.institucion_direccion}
             institucion_zona={medico.institucion_zona}
-            instituciones={getInstitutionsForProvider(medico.prestador_apellido + ' ' + medico.prestador_nombre)}
-            centro={medico.institucion_nombre}
+            instituciones={medico.prestador_apellido + ' ' + medico.prestador_nombre}
+            centro={medico.institucion_nombre ? medico.institucion_nombre : medico.prestador_apellido+ ' ' + medico.prestador_nombre}
           />
         )
       ))}
     </div>
-  )}
-</div>
-
+    </div>
   );
 };
 
