3e600a44823ddb2502835919deaadbd13fef5c4c
Leandro <leandro.david.dini@gmail.com>
2023-10-25 08:13:06 -0300

    filtro modificado

--- src/Components/CardsConteiner/CarsConteiner.jsx
+++ src/Components/CardsConteiner/CarsConteiner.jsx
@@ -17,11 +17,19 @@ const FetchComponent = () => {
   const [uniqueProviders, setUniqueProviders] = useState([]);
 
 
-
 useEffect(() => {
-  fetch('http://localhost:3000/prestador/detail')
+  fetch('http://107.180.104.190:7012/prestador/detail')
     .then(response => response.json())
     .then(jsonData => {
+      jsonData.sort((a, b) => {
+        if (a.institucion_nombre < b.institucion_nombre) {
+          return -1;
+        }
+        if (a.institucion_nombre > b.institucion_nombre) {
+          return 1;
+        }
+        return 0;
+      });
       setData(jsonData);
       setFilteredData([]);
       const typesInData = [...new Set(jsonData.map(item => item.institucion_tipo))];
@@ -31,6 +39,7 @@ useEffect(() => {
     })
     .catch(error => console.error('Error fetching data:', error));
 }, []);
+
 
   useEffect(() => {
     const typesInSelectedZone = selectedZone === 'all' ? [...new Set(data.map(item => item.institucion_tipo))] : [...new Set(data.filter(item => item.institucion_zona === selectedZone).map(item => item.institucion_tipo))];
@@ -47,15 +56,6 @@ useEffect(() => {
     setUniqueTypes(typesInSelectedZone);
   }, [selectedZone, data]);
   
-  useEffect(() => {
-    let uniqueInstitutionsByType = [];
-    if (selectedZone !== 'all') 
-      uniqueInstitutionsByType = selectedType !== '' ? [...new Set(data.filter(item => item.institucion_tipo === selectedType).map(item => item.institucion_nombre))] : [];
-    else{
-      uniqueInstitutionsByType = selectedType !== '' ? [...new Set(data.map(item => item.institucion_nombre))] : [];
-    }
-      setUniqueInstitutions(uniqueInstitutionsByType);
-  }, [selectedZone,selectedType, data]);
 
   useEffect(() => {
     let uniqueInstitutionsByType = [];
@@ -76,29 +76,74 @@ useEffect(() => {
   }, [selectedZone, selectedType, data]);
   
 
-useEffect(() => {
-  let specialtiesInSelectedInstitution = [];
-  if (selectedInstitution !== '') {
-    if (selectedInstitution === 'all') {
-      specialtiesInSelectedInstitution = [...new Set(data.map(item => item.especialidad_nombre))];
-    } else {
-      specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_nombre === selectedInstitution).map(item => item.especialidad_nombre))];
+  useEffect(() => {
+    let specialtiesInSelectedInstitution = [];
+  
+    switch (true) {
+      case (selectedInstitution === 'all' && selectedType === 'all' && selectedZone === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.map(item => item.especialidad_nombre))];
+        break;
+      case (selectedInstitution === 'all' && selectedType === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_zona === selectedZone).map(item => item.especialidad_nombre))];
+        break;
+      case (selectedInstitution === 'all' && selectedZone === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_tipo === selectedType).map(item => item.especialidad_nombre))];
+        break;
+      case (selectedType === 'all' && selectedZone === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_nombre === selectedInstitution).map(item => item.especialidad_nombre))];
+        break;
+      case (selectedInstitution === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_tipo === selectedType && item.institucion_zona === selectedZone).map(item => item.especialidad_nombre))];
+        break;
+      case (selectedType === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone).map(item => item.especialidad_nombre))];
+        break;
+      case (selectedZone === 'all'):
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType).map(item => item.especialidad_nombre))];
+        break;
+      default:
+        specialtiesInSelectedInstitution = [...new Set(data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType && item.institucion_zona === selectedZone).map(item => item.especialidad_nombre))];
+        break;
     }
-  }
-  setUniqueSpecialties(specialtiesInSelectedInstitution);
-}, [selectedInstitution, data]);
+    setUniqueSpecialties(specialtiesInSelectedInstitution);
+  }, [selectedInstitution, selectedType, selectedZone, data]);
+  
 
 useEffect(() => {
   let providersInSelectedSpecialty = [];
-  if (selectedSpecialty !== '') {
-    if (selectedSpecialty === 'all') {
+  switch (true) {
+    case (selectedInstitution === 'all' && selectedType === 'all' && selectedZone === 'all' && selectedSpecialty === ''):
       providersInSelectedSpecialty = [...new Set(data.map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
-    } else {
+      break;
+    case (selectedInstitution === 'all' && selectedType === 'all' && selectedZone === 'all'):
       providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
-    }
+      break;
+    case (selectedInstitution === 'all' && selectedType === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_zona === selectedZone).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    case (selectedInstitution === 'all' && selectedZone === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_tipo === selectedType).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    case (selectedType === 'all' && selectedZone === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_nombre === selectedInstitution).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    case (selectedInstitution === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_tipo === selectedType && item.institucion_zona === selectedZone).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    case (selectedType === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    case (selectedZone === 'all'):
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
+    default:
+      providersInSelectedSpecialty = [...new Set(data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType && item.institucion_zona === selectedZone).map(item => `${item.prestador_nombre} ${item.prestador_apellido}`))];
+      break;
   }
   setUniqueProviders(providersInSelectedSpecialty);
-}, [selectedSpecialty, data]);
+}, [selectedZone,selectedType,selectedSpecialty,selectedInstitution, data]);
+
+
 
   
   const handleZoneFilter = (zone) => {
@@ -134,15 +179,19 @@ useEffect(() => {
   
   const handleInstitutionFilter = (institution) => {
     let filtered = [];
-
-    if (selectedZone === 'all' && selectedType === 'all') {
-      filtered = institution === 'all' ? [...data] : data.filter(item => item.institucion_nombre === institution);
-    } else if (selectedZone === 'all') {
-      filtered = institution === 'all' ? data.filter(item => item.institucion_tipo === selectedType) : data.filter(item => item.institucion_tipo === selectedType && item.institucion_nombre === institution);
-    } else if (selectedType === 'all') {
-      filtered = institution === 'all' ? data.filter(item => item.institucion_zona === selectedZone) : data.filter(item => item.institucion_zona === selectedZone && item.institucion_nombre === institution);
-    } else {
-      filtered = institution === 'all' ? data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType) : data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.institucion_nombre === institution);
+    switch (true) {
+      case (selectedZone === 'all' && selectedType === 'all'):
+        filtered = institution === 'all' ? [...data] : data.filter(item => item.institucion_nombre === institution);
+        break;
+      case (selectedZone === 'all'):
+        filtered = institution === 'all' ? data.filter(item => item.institucion_tipo === selectedType) : data.filter(item => item.institucion_tipo === selectedType && item.institucion_nombre === institution);
+        break;
+      case (selectedType === 'all'):
+        filtered = institution === 'all' ? data.filter(item => item.institucion_zona === selectedZone) : data.filter(item => item.institucion_zona === selectedZone && item.institucion_nombre === institution);
+        break;
+      default:
+        filtered = institution === 'all' ? data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType) : data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.institucion_nombre === institution);
+        break;
     }
     setFilteredData(filtered);
     setSelectedInstitution(institution);
@@ -150,25 +199,79 @@ useEffect(() => {
     setSelectedProvider('');
   };
   
+  
 
   const handleSpecialtyFilter = (specialty) => {
     let filtered = [];
-  
-    if (selectedInstitution === 'all') {
-      filtered = specialty === 'all' ? [...data] : data.filter(item => item.especialidad_nombre === specialty);
-    } else {
-      filtered = specialty === 'all' ? data.filter(item => item.institucion_nombre === selectedInstitution) : data.filter(item => item.institucion_nombre === selectedInstitution && item.especialidad_nombre === specialty);
+    switch (true) {
+      case (selectedInstitution === 'all' && selectedZone === 'all' && selectedType === 'all'):
+        filtered = data.filter(item => item.especialidad_nombre === specialty);
+        break;
+      case (selectedInstitution === 'all' && selectedZone === 'all'):
+        filtered =  data.filter(item => item.institucion_tipo === selectedType && item.especialidad_nombre === specialty);
+        break;
+      case (selectedInstitution === 'all' && selectedType === 'all'):
+        filtered =  data.filter(item => item.institucion_zona === selectedZone && item.especialidad_nombre === specialty);
+        break;
+      case (selectedZone === 'all' && selectedType === 'all'):
+        filtered =  data.filter(item => item.institucion_nombre === selectedInstitution && item.especialidad_nombre === specialty);
+        break;
+      case (selectedInstitution === 'all'):
+        filtered =  data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.especialidad_nombre === specialty);
+        break;
+      case (selectedZone === 'all'):
+        filtered =  data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType && item.especialidad_nombre === specialty);
+        break;
+      case (selectedType === 'all'):
+        filtered =  data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone && item.especialidad_nombre === specialty);
+        break;
+      default:
+        filtered = specialty === 'all' ? data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone && item.institucion_tipo === selectedType) : data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.especialidad_nombre === specialty);
+        break;
     }
     setFilteredData(filtered);
     setSelectedSpecialty(specialty);
     setSelectedProvider('');
   };
 
+
   const handleProviderFilter = (provider) => {
-    const filtered = data.filter(item => `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+    let filtered = [];
+    switch (true) {
+      case (selectedInstitution === 'all' && selectedZone === 'all' && selectedType === 'all' && selectedSpecialty === ''):
+        filtered = data.filter(item => `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedInstitution === 'all' && selectedZone === 'all' && selectedType === 'all'):
+        filtered = data.filter(item => item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedInstitution === 'all' && selectedZone === 'all'):
+        filtered = data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_tipo === selectedType && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedInstitution === 'all' && selectedType === 'all'):
+        filtered = data.filter(item => item.especialidad_nombre === selectedSpecialty && item.institucion_zona === selectedZone && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedZone === 'all' && selectedType === 'all'):
+        filtered = data.filter(item => item.institucion_nombre === selectedInstitution && item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedInstitution === 'all'):
+        filtered = data.filter(item => item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedZone === 'all'):
+        filtered = data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_tipo === selectedType && item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      case (selectedType === 'all'):
+        filtered = data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone && item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+      default:
+        filtered = data.filter(item => item.institucion_nombre === selectedInstitution && item.institucion_zona === selectedZone && item.institucion_tipo === selectedType && item.especialidad_nombre === selectedSpecialty && `${item.prestador_nombre} ${item.prestador_apellido}` === provider);
+        break;
+    }
+    setSelectedProvider('');
     setFilteredData(filtered);
     setSelectedProvider(provider);
   };
+  
+  
 
   const uniqueZones = [...new Set(data.map(item => item.institucion_zona))];
 
@@ -176,8 +279,8 @@ useEffect(() => {
 <div>
   <div>
     <select className="form-select select-style" value={selectedZone} onChange={(e) => handleZoneFilter(e.target.value)}>
-      <option value="">-- Seleccione una zona --</option>
-      <option value="all">Todas las Zonas</option>
+      <option value="">Seleccione una zona</option>
+      <option value="all">Todas las zonas</option>
       {uniqueZones.map((zone, index) => (
         <option key={index} value={zone}>
           {zone}
@@ -187,19 +290,19 @@ useEffect(() => {
   </div>
   <div>
     <select className="form-select select-style" value={selectedType} onChange={(e) => handleTypeFilter(e.target.value)} disabled={!selectedZone}>
-      <option value="">-- Seleccione tipo --</option>
-      <option value="all">Todos los Tipos</option>
+      <option value="">Seleccione tipo</option>
+      <option value="all">Todos los tipos</option>
       {uniqueTypes.map((type, index) => (
         <option key={index} value={type}>
-          {type === 'cabecera' ? 'Medico de Cabecera' : type === 'Clinica' ? 'Opción 2' : 'Centro Medico'}
+          {type}
         </option>
       ))}
     </select>
   </div>
   <div>
     <select className="form-select select-style" value={selectedInstitution} onChange={(e) => handleInstitutionFilter(e.target.value)} disabled={!selectedType || !selectedZone}>
-      <option value="">-- Seleccione institucion --</option>
-      <option value="all">Todas las Instituciones</option>
+      <option value="">Seleccione institucion</option>
+      <option value="all">Todas las instituciones</option>
       {selectedType && selectedZone && uniqueInstitutions.map((institution, index) => (
         <option key={index} value={institution}>
           {institution}
@@ -209,7 +312,7 @@ useEffect(() => {
   </div>
   <div>
     <select className="form-select select-style" value={selectedSpecialty} onChange={(e) => handleSpecialtyFilter(e.target.value)} disabled={!selectedInstitution}>
-      <option value="">-- Seleccione especialidad --</option>
+      <option value="">Seleccione especialidad</option>
       {selectedInstitution && uniqueSpecialties.map((specialty, index) => (
         <option key={index} value={specialty}>
           {specialty}
@@ -219,7 +322,7 @@ useEffect(() => {
   </div>
   <div>
     <select className="form-select select-style" value={selectedProvider} onChange={(e) => handleProviderFilter(e.target.value)} disabled={!selectedSpecialty}>
-      <option value="">-- Seleccione proveedores --</option>
+      <option value="">Seleccione proveedores</option>
       {selectedSpecialty && uniqueProviders.map((provider, index) => (
         <option key={index} value={provider}>
           {provider}
@@ -235,7 +338,7 @@ useEffect(() => {
         apellido={medico.prestador_nombre}
         especialidad_nombre={medico.especialidad_nombre}
         horarios_trabajo={medico.horarios_trabajo}
-        telefono_numero={medico.telefonos}
+        telefono_numero={medico.telefonos ? medico.telefonos : ''}
         institucion_direccion={medico.institucion_direccion}
         institucion_zona={medico.institucion_zona}
         instituciones={`${medico.prestador_apellido} ${medico.prestador_nombre}`}
